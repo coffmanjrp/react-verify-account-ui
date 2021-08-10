61c36868e34675b93bd08420bf67cfab73c73749
coffmanjrp <coffmanjrp@gmail.com>
2021-08-10 01:09:44 -0700

    Implement verify function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useRef } from 'react';
 import './App.scss';
 
 function App() {
+  const ref = useRef();
+
+  useEffect(() => {
+    const codes = ref.current.childNodes;
+
+    codes[0].focus();
+
+    codes.forEach((code, index) => {
+      code.addEventListener('keydown', (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+          codes[index].value = '';
+          setTimeout(() => codes[index + 1]?.focus(), 10);
+        } else if (e.key === 'Backspace') {
+          setTimeout(() => codes[index - 1]?.focus(), 10);
+        }
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -9,55 +29,18 @@ function App() {
           We emailed you the six digit code to example@email.com <br />
           Enter the code below to confirm your email address.
         </p>
-        <div className="code-container">
-          <input
-            type="number"
-            className="code"
-            placeholder="0"
-            min="0"
-            max="9"
-            required
-          />
-          <input
-            type="number"
-            className="code"
-            placeholder="0"
-            min="0"
-            max="9"
-            required
-          />
-          <input
-            type="number"
-            className="code"
-            placeholder="0"
-            min="0"
-            max="9"
-            required
-          />
-          <input
-            type="number"
-            className="code"
-            placeholder="0"
-            min="0"
-            max="9"
-            required
-          />
-          <input
-            type="number"
-            className="code"
-            placeholder="0"
-            min="0"
-            max="9"
-            required
-          />
-          <input
-            type="number"
-            className="code"
-            placeholder="0"
-            min="0"
-            max="9"
-            required
-          />
+        <div ref={ref} className="code-container">
+          {[...Array(6)].map((_, index) => (
+            <input
+              key={index}
+              type="number"
+              className="code"
+              placeholder="0"
+              min="0"
+              max="9"
+              required
+            />
+          ))}
         </div>
         <small className="info">
           This is design only. We didn't actually send you an email as we don't
